7b42f42dbe5df628a6ce0fdeeccf85bc214cf23f
jackyrwj <763214954@qq.com>
2025-06-09 01:16:24 +0800

    fix

--- scripts/update-readme.sh
+++ scripts/update-readme.sh
@@ -13,7 +13,7 @@ RANDOM_IMAGE=${IMAGES[$RANDOM % ${#IMAGES[@]}]}
 # 复制随机图片为 current.png
 cp "$RANDOM_IMAGE" "images/current.png"
 
-# 更新 README.md，添加时间戳和随机数
+# 更新 README.md
 TIMESTAMP=$(date +%s)
 RANDOM_NUM=$((RANDOM % 1000))
 sed -i "s|src='./images/current.png'|src='./images/current.png?v=${TIMESTAMP}_${RANDOM_NUM}'|" README.md
